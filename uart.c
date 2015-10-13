02b72bed755b4e2ecdf6772c86167a149b030ada
Eugene Samoylov <e.samoylov@2Gis.ru>
2015-10-13 22:39:54 +0600

    some refactoring

--- uart.c
+++ uart.c
@@ -3,9 +3,18 @@
 
 #include "uart.h"
 
-#define BAUDRATE 19200                                         //The baudrate that we want to use
-#define BAUD_PRESCALLER (((F_CPU / (BAUDRATE * 16UL))) - 1)    //The formula that does all the required maths
+#define BAUD_PRESCALLER (((F_CPU / (BAUDRATE * 16UL))) - 1)
 
+int uart_putchar(char c, FILE *stream) {
+	while(!(UCSRA & (1<<UDRE)));
+	UDR = c;
+	return 0;
+}
+
+int uart_getchar(FILE *sttream) {
+	while(!(UCSRA & (1<<RXC)));
+	return UDR;
+}
 
 FILE uart_output = FDEV_SETUP_STREAM(uart_putchar, NULL, _FDEV_SETUP_WRITE);
 FILE uart_input = FDEV_SETUP_STREAM(NULL, uart_getchar, _FDEV_SETUP_READ);
@@ -22,13 +31,3 @@ void uart_init(void) {
 	stdin  = &uart_input;
 }
 
-int uart_putchar(char c, FILE *stream) {
-	while(!(UCSRA & (1<<UDRE)));
-	UDR = c;
-	return 0;
-}
-
-int uart_getchar(FILE *sttream) {
-	while(!(UCSRA & (1<<RXC)));
-	return UDR;
-}
--- uart.h
+++ uart.h
@@ -4,8 +4,8 @@
 #include <avr/io.h>
 #include <stdio.h>
 
+#define BAUDRATE 19200     //baudrate (18n)
+
 void uart_init(void);
-int uart_putchar(char c, FILE *stream);
-int uart_getchar(FILE *sttream);
 
 #endif /* defined _uart_h */
