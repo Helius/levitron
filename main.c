58fa7f228a72620f41d6178d80fa462d1ed57d86
Eugene Samoylov <eugene.samylov@eltex.loc>
2015-10-31 21:46:00 +0600

    work 1

--- main.c
+++ main.c
@@ -36,12 +36,13 @@ void pwm_init()
 	TCCR1A = (1<<COM1A1) | (1<<WGM12) | (1<<WGM11) | (0<<WGM10); // fast pwm with ICR1 top
 	TCCR1B = (1<< WGM13) | (1<<CS10);   // clk/1
 	
-	OCR1A = 6000;
-	ICR1 =  10000;
+	OCR1A = 300;
+	ICR1 =  1000;
 }
 
 
 Result_Buffer sensor_buf[2];
+//int adjust = 0;
 
 void add_result(int ind, int result)
 {
@@ -55,17 +56,15 @@ ISR(ADC_vect)
 	if (channel == S_UP) {
 		add_result(0, ADC);
 		channel = S_DOWN;
-	} else {
+	} else if (channel == S_DOWN){
 		add_result(1, ADC);
+		channel = CALIB_CHAN;
+	} else {
+	//	adjust = ADC;
 		channel = S_UP;
 	}
 	adc_start(channel);
 	//TGLBIT(PORTB,0);
-}
-
-ISR(TIMER0_OVF_vect)
-{
-	do_levitate();
 }
 
 int filter_signal(int ind)
@@ -78,27 +77,54 @@ int filter_signal(int ind)
 
 void change_pwm(int value)
 {
-	int cur = value*95;
+	int cur = value*9;
 	OCR1A = cur;
 }
 
 
-#define K 1
 int new = 0;
 int diff = 0;
+int K = 5;
+int Kd = 10;
+int S = 0;
+
+int abs (int val)
+{
+	return (val>0) ? val : -val;
+}
 
 void do_levitate()
 {
 	SETBIT(PORTB,0);
 	int up = filter_signal(0);
 	int dn = filter_signal(1);
-	diff = dn-up-70;
-	new = 60 - (K*diff);
+	diff = dn-up-100;
+/*
+	if (abs(diff) < 6)
+		K = 15;
+	else if (abs(diff) < 10)
+		K = 1;
+	else if (abs(diff) < 20)
+		K = 1;
+*/
+
+	static int cnt = 0;
+	static int prevDiff = 0;
+	if (++cnt > 15) {
+		cnt = 0;
+		S = diff - prevDiff;
+		prevDiff = diff;
+	}
+
+	if (diff < -50)
+		return;
+
+	new = 30 - (K*diff)/5 - (Kd*S)/10;
 	
-	//if (new > 80)
-	//	new = 50;
 	if (new < 0)
 		new = 0;
+	if (new > 50)
+		return;
 	change_pwm(new);
 	CLRBIT(PORTB,0);
 }
@@ -110,21 +136,28 @@ int main(void)
 	DDRB |= 3;
 	PORTB |= 1;
 
+	SETBIT(PORTB,0);
+
 
 	uart_init();
+	printf("Levitron collider started...\n\r");
 	pwm_init();
-	adc_init();
+	//adc_init();
 	timer_init();
 	sei();
 
-	printf("Levitron collider started...\n\r");
 	
 	while (1) {
 		up = filter_signal(0);
 		dn = filter_signal(1);
-		printf("adc:up:%d dn:%d diff:%d\t diff=%d, new=%d\n\r", up, dn, dn-up, diff, new);
-		_delay_ms(300);		
+		printf("adc:up:%d dn:%d diff:%d\t diff=%d,S=%d\tK=%d, new=%d\n\r", up, dn, dn-up, diff, S, K, new);
+		_delay_ms(30);		
 		//TGLBIT(PORTB,0);
 	}
 	return 0;
+}
+
+ISR(TIMER0_OVF_vect)
+{
+	do_levitate();
 }
